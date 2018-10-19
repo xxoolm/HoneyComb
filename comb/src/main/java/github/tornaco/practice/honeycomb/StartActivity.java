715a5edcfadce72f003bc6f0345ee0d85adaf3bc
Hao Guo <hguo@thoughtworks.com>
2018-10-19 17:17:25 +0800

    Init comb

---
+++ comb/src/main/java/github/tornaco/practice/honeycomb/StartActivity.java
@@ -0,0 +1,14 @@
+package github.tornaco.practice.honeycomb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
